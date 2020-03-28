8217ee5136464a6994db81061539bd76293575d4
Orji Cecilia <ceciliaorji.co@gmail.com>
2020-03-28 15:05:36 +0100

    git.js

---
+++ git.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+<script>
+
+let name = "Orji Cecilia Olanma";
+console.log("My name is " + name);
+
+const emailAddress = "ceciliaorji.co@gmail.com";
+console.log("My email-address is " + emailAddress);
+
+let slackUsername = "vivcis"
+console.log("My slack username is " + slackUsername);
+
+const track = "Front-end";
+console.log("The track I am offering is " + track);
+
+var courses = ["Front-end", "Coding"];
+console.log("My couses are " + courses);
+
+</script>
+</body>
+</html>
